c9df3f2298869b8247bd22e31eeb0088d5464736
Kaustubh Chaubey <109577773+ckaustubh23@users.noreply.github.com>
2024-09-09 16:04:02 +0530

    Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace MyMvcApp.Models
+namespace StoreManagementSystem.Models
 {
     public class Product
     {
