8a1a38aaef162873f4a8373d22adad9930b31dd2
Kaustubh Chaubey <109577773+ckaustubh23@users.noreply.github.com>
2024-09-09 16:04:23 +0530

    Update Purchase.cs

--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -2,7 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MyMvcApp.Models
+namespace StoreManagementSystem.Models
 {
     public class Purchase
     {
